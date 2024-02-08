7efe80e7d3b713681004a20204497f491992799c
Lucas Navajas <lucastobiasnavajas@gmail.com>
2024-02-08 11:11:08 -0300

    Translate Finished

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LanguageService } from '../language.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  translatedText: string = '';
+  constructor(private languageService: LanguageService, private translateService: TranslateService) { }
 
+  ngOnInit(): void {
+    this.languageService.languageChange$.subscribe(language => {
+      // Cuando cambia el idioma, traduce el texto
+      this.translateService.get('yourTranslationKey').subscribe((translatedText: string) => {
+        // Asigna el texto traducido a la variable translatedText
+        this.translatedText = translatedText;
+      });
+    });
+  }
 }
